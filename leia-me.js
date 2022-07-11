92f773015818fade3915f3a8eea07300fec2f265
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2022-07-11 19:07:44 -0300

    a

---
+++ leia-me.js
@@ -0,0 +1,19 @@
+//                         Project by -Group.
+//                    main/master - version 2.10.2 --realiases
+
+// author project API: vitor_xp.      -Projeto mesa de trabalho.
+
+
+// Pronto, antes de tudo instale o que se pede a baixo:
+nodejs = "https://nodejs.org/en/" //onde a magia acontece.
+vs_code = "https://code.visualstudio.com/Download" //nossa plataforma onde ira rodar o projeto.
+
+// Após ter baixado e aberto o VSCode, precione [Ctrl] + [Shift] + ["] para abrir o terminal.
+// Execute "node" e precione [Enter], em seguida irá aparecer a versão+help, caso não aparecer, verifique se você reiniciou o VSCode.
+
+// Após fazer isto, execute "npm i" para instalar todas as dependencias do projeto.
+// Depois de "node ." para executar o projeto.
+
+
+// 'Ue, onde estão os arquivos?'
+// Os arquivos estão na pasta "src", a principal como sempre e a "index.js".
